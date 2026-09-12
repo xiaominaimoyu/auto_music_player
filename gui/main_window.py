768e86939faa0b3d515c4cf1155cb2d0e93352bb
xiaominaimoyu <xiaominaimoyu@users.noreply.github.com>
2026-09-12 11:50:10 +0800

    fix: update desktop UI version to v1.2.2

--- gui/main_window.py
+++ gui/main_window.py
@@ -276,7 +276,7 @@ class MainWindow(QMainWindow):
         self.nav.currentRowChanged.connect(self._switch_page)
         sidebar_layout.addWidget(self.nav, 1)
 
-        footer = QLabel("v1.1")
+        footer = QLabel("v1.2.2")
         footer.setObjectName("SidebarFooter")
         sidebar_layout.addWidget(footer)
 
